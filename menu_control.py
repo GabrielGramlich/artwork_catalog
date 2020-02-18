b5f33f18d12111df2e78e110095eaba77a939689
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 06:52:44 -0600

    Add import from get_input

--- menu_control.py
+++ menu_control.py
@@ -1,30 +1,10 @@
 from artist import Artist
 from artwork import Artwork
+from get_input import *
 
 def display_menu():
     print() # useless command to have something in my method
     ##TODO Create display menu method
-
-
-def get_user_input(instructions, input_command):
-    print(instructions)
-    choice = input(input_command)
-    return choice
-
-
-def get_user_input_with_two(instructions, input_command_one, input_command_two):
-    print(instructions)
-    choice_one = input(input_command_one)
-    choice_two = input(input_command_one)
-    return choice_one, choice_two
-
-
-def get_user_input_with_three(instructions, input_command_one, input_command_two, input_command_three):
-    print(instructions)
-    choice_one = input(input_command_one)
-    choice_two = input(input_command_one)
-    choice_three = input(input_command_three)
-    return choice_one, choice_two, choice_three
 
 
 def create_artist():
