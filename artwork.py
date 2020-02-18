1f5cb35ca03da2fe189d0b2e78861ea491d1dd53
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 06:34:51 -0600

    Create artwork object class

---
+++ artwork.py
@@ -0,0 +1,11 @@
+class Artwork:
+
+	def __init__(self, artist, name, price, availability):
+		self.artist = artist
+		self.artwork_name = name
+		self.price = price
+		self.availability = availability
+
+	def __str__(self):
+		artist_info = 'Artwork Information:\nArtist:\t\t' + self.artist + '\nName:\t\t' + self.artwork_name + '\nPrice:\t\t' + self.price + 'Available:\t' + self.availability + '\n'
+		return artist_info
