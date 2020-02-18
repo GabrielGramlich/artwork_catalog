deb2680ef3a774972e914ac04270d51c3ac7398e
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 04:24:35 -0600

    Update method names to undo overloading

--- menu_control.py
+++ menu_control.py
@@ -9,14 +9,14 @@ def get_user_input(instructions, input_command):
     return choice
 
 
-def get_user_input(instructions, input_command_one, input_command_two):
+def get_user_input_with_two(instructions, input_command_one, input_command_two):
     print(instructions)
     choice_one = input(input_command_one)
     choice_two = input(input_command_one)
     return choice_one, choice_two
 
 
-def get_user_input(instructions, input_command_one, input_command_two, input_command_three):
+def get_user_input_with_three(instructions, input_command_one, input_command_two, input_command_three):
     print(instructions)
     choice_one = input(input_command_one)
     choice_two = input(input_command_one)
