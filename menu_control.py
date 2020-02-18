600a7c825e5f7ce0b734626a554250d96291f678
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 06:40:54 -0600

    Get artwork object

--- menu_control.py
+++ menu_control.py
@@ -1,4 +1,5 @@
 from artist import Artist
+from artwork import Artwork
 
 def display_menu():
     print() # useless command to have something in my method
@@ -26,18 +27,16 @@ def get_user_input_with_three(instructions, input_command_one, input_command_two
     return choice_one, choice_two, choice_three
 
 
-def create_artist(artist):
+def create_artist():
     name, email = get_user_input_with_two('Please insert artist information.', 'Name: ', 'Email: ')
     artist = Artist(name, email)
     database_control.create_artist(artist)
-    ##TODO Create method to create artist
-    ##      Calls database_control
 
 
-def create_artwork(artwork):
-    print() # useless command to have something in my method
-    ##TODO Create method to create artwork
-    ##      Calls database_control
+def create_artwork():
+    artist, name, price = get_user_input_with_three('Please insert artwork information.', 'Artist: ', 'Name: ', 'Price: ')
+    artwork = Artwork(artist, name, price)
+    database_control.create_artwork(artwork)
 
 
 def search_artwork(artist):
