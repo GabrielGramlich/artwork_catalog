78f3cf78d4aa351fc305f423d4df558e29952f66
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 04:47:28 -0600

    Store artist info in object

---
+++ artist.py
@@ -0,0 +1,10 @@
+class Menu:
+
+	def __init__(self, name, email):
+		self.name = name
+		self.email = email
+
+
+	def __str__(self):
+		artist_info = 'Artist Information:\nName:\t' + name + '\nEmail:\t' + email + '\n'
+		return artist_info
