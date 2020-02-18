490df6931ca74bfb0b2ca66931e08fb31d5a9b2f
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 03:24:30 -0600

    Move TODO items to methods

--- menu_control.py
+++ menu_control.py
@@ -1,13 +1,51 @@
-##TODO Create display menu method
-##TODO Create user input method
-##      overload for multiple inputs: one for artist, artwork creation, and artwork search.
-##TODO Create method to create artist
-##      Calls database_control
-##TODO Create method to create artwork
-##      Calls database_control
-##TODO Create method to search artwork
-##      Calls database_control
-##TODO Create method to update artwork
-##      Calls database_control
-##TODO Create method to delete artwork
-##      Calls database_control
+def display_menu():
+    print() # useless command to have something in my method
+    ##TODO Create display menu method
+
+
+def get_user_input(input_command):
+    print() # useless command to have something in my method
+    ##TODO Create user input method
+    ##      overload for multiple inputs: one for artist, artwork creation, and artwork search.
+
+
+def get_user_input(input_command_one, input_command_two):
+    print() # useless command to have something in my method
+    ##TODO Create user input method
+    ##      overload for multiple inputs: one for artist, artwork creation, and artwork search.
+
+
+def get_user_input(input_command_one, input_command_two, input_command_three):
+    print() # useless command to have something in my method
+    ##TODO Create user input method
+    ##      overload for multiple inputs: one for artist, artwork creation, and artwork search.
+
+
+def create_artist(artist):
+    print() # useless command to have something in my method
+    ##TODO Create method to create artist
+    ##      Calls database_control
+
+
+def create_artwork(artwork):
+    print() # useless command to have something in my method
+    ##TODO Create method to create artwork
+    ##      Calls database_control
+
+
+def search_artwork(artist):
+    print() # useless command to have something in my method
+    ##TODO Create method to search artwork
+    ##      Calls database_control
+
+
+def update_artwork(artwork, availability):
+    print() # useless command to have something in my method
+    ##TODO Create method to update artwork
+    ##      Calls database_control
+
+
+def delete_artwork(artwork):
+    print() # useless command to have something in my method
+    ##TODO Create method to delete artwork
+    ##      Calls database_control
