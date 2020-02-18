d731225d4d78080c0636ecda5966b3d419f013bb
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 12:45:46 -0600

    Convert all input to uppercase

--- get_input.py
+++ get_input.py
@@ -1,21 +1,21 @@
 def get_user_input(instructions, input_command):
     print(instructions)
-    choice = input(input_command)
+    choice = input(input_command).upper()
     return choice
 
 
 def get_user_input_with_two(instructions, input_command_one, input_command_two):
     print(instructions)
-    choice_one = input(input_command_one)
-    choice_two = input(input_command_one)
+    choice_one = input(input_command_one).upper()
+    choice_two = input(input_command_one).upper()
     return choice_one, choice_two
 
 
 def get_user_input_with_three(instructions, input_command_one, input_command_two, input_command_three):
     print(instructions)
-    choice_one = input(input_command_one)
-    choice_two = input(input_command_one)
-    choice_three = input(input_command_three)
+    choice_one = input(input_command_one).upper()
+    choice_two = input(input_command_one).upper()
+    choice_three = input(input_command_three).upper()
     return choice_one, choice_two, choice_three
 
 
