8e1bbd8e70a0efb934f205ace7c44fd1da71dba5
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 07:29:12 -0600

    Add tables to database

--- database_control.py
+++ database_control.py
@@ -1,3 +1,36 @@
+from peewee import *
+
+db = SqliteDatabase('artwork_catalog.sqlite')
+
+
+class Artist_Table(Model):
+    self.name = CharField()
+    self.email = CharField()
+
+    class Meta:
+        database = db
+
+
+    def __str__(self):
+        return f'Artist:\t{self.name}\nEmail:\t{self.email}\n'
+
+
+class Artwork_Table(Model):
+    self.artist = CharField()
+    self.artwork = CharField()
+    self.price = DecimalField()
+    self.availability = BooleanField()
+
+    class Meta:
+        database = db
+
+
+    def __str__(self):
+        return f'Artist:\t\t{self.name}\nArtwork:\t\t{self.artwork}\nPrice:\t\t{self.price}\nAvailability:\t{self.availability}\n'
+
+
+db.connect()
+db.create_tables([Artist_Table, Artwork_Table])
 ##TODO Create Database
 ##TODO Create artist table
 ##      Artist table has name and email address
