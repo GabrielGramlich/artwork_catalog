543e4f8c4f1bc6030a245efcb6421dc282260032
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 04:27:13 -0600

    Create menu when program runs

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from menu_control import get_user_input
+from menu import Menu
 
 def main():
+	menu = create_menu()
+
 	while True:
 		choice = get_user_input('Enter \'m\' to view menu, or enter your choice.', 'What would you like to do? ')
 		if choice == 'q':
 			break
 		else:
 			call_for_choice(choice)
+
+
+def create_menu():
+	menu = Menu()
+
+	return menu
 
 
 def call_for_choice(choice):
