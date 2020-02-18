901a64b068bd622ab1644ae71405b293291f2151
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-18 10:46:19 -0600

    Add methods

--- database_control.py
+++ database_control.py
@@ -1,10 +1,27 @@
-##TODO Create Database
-##TODO Create artist table
-##      Artist table has name and email address
-##TODO Create artrowk table
-##      Artwork table has artist(FK), artwork name, price(float), availability(tinyint)
-##TODO Create add artist method
-##TODO Create add artwork method - create artist if not in database
-##TODO Create search artwork method by artist
-##TODO Create delete artwork method
-##TODO Create update availability status method
+from peewee import *
+
+db = SqliteDatabase('artwork_catalog.sqlite')
+
+def create_artist(artist):
+    print() # Useless line to have something in my method
+    ##TODO Create add artist method
+
+
+def create_artwork(artwork):
+    print() # Useless line to have something in my method
+    ##TODO Create add artwork method - create artist if not in database
+
+
+def search_artwork(name):
+    print() # Useless line to have something in my method
+    ##TODO Create search artwork method by artist
+
+
+def update_artwork(artist_name, artwork_name, availability):
+    print() # Useless line to have something in my method
+    ##TODO Create delete artwork method
+
+
+def delete_search(artist_name, artwork_name):
+    print() # Useless line to have something in my method
+    ##TODO Create update availability status method
