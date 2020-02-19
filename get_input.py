4857a6af9278172116646756178dc87f82f0c1db
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-02-19 08:30:12 -0600

    Fix output for user instructions

--- get_input.py
+++ get_input.py
@@ -7,14 +7,14 @@ def get_user_input(instructions, input_command):
 def get_user_input_with_two(instructions, input_command_one, input_command_two):
     print(instructions)
     choice_one = input(input_command_one).upper()
-    choice_two = input(input_command_one).upper()
+    choice_two = input(input_command_two).upper()
     return choice_one, choice_two
 
 
 def get_user_input_with_three(instructions, input_command_one, input_command_two, input_command_three):
     print(instructions)
     choice_one = input(input_command_one).upper()
-    choice_two = input(input_command_one).upper()
+    choice_two = input(input_command_two).upper()
     choice_three = input(input_command_three).upper()
     return choice_one, choice_two, choice_three
 
